eea10edb1c7a8d9ab05ec4b058c9d71eeeb21b97
yysyp <yysyp@foxmail.com>
2023-10-17 14:41:10 +0800

    modify redeploy scriopt

--- deployment-scripts/vm-to-deploy/deploy_folder/5-1app-redeploy-restart.sh
+++ deployment-scripts/vm-to-deploy/deploy_folder/5-1app-redeploy-restart.sh
@@ -3,13 +3,21 @@ set -o nounset
 set -o errexit
 #sed -i 's/\r$//' *.sh
 
-APP_FILE_NAME=xxx
-cp app1-xx.jar app-redeploy-start.sh /usr/local/app1/
-cd /usr/local/app1
-ps -ef | grep APP_FILE_NAME | grep -v grep | awk '{print $2}' | xargs kill -9
+APP_FILE_NAME=simple-e-commerce-v2-1.0.0
+
+if [[ "$PWD" == "/usr/local/app1"]]; then
+  echo ""
+else
+  echo "copy."
+  cp $APP_FILE_NAME.jar 5-1app-redeploy-restart.sh /usr/local/app1/
+  cd /usr/local/app1
+fi
+#ps -ef | grep APP_FILE_NAME | grep -v grep | awk '{print $2}' | xargs kill -9
 #ps -aux | grep APP_FILE_NAME | grep -v grep | awk '{print $2}' | xargs kill -9
 #kill -s 9 `pgrep APP_FILE_NAME`
-nohup java -server -XX:+PrintGCDetails -XX:+PrintGCTimeStamps \
--Xloggc:./log/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 \
--XX:GCLogFileSize=1024k -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$remotefolder \
--Dspring.profiles.active=dev -jar $APP_FILE_NAME > /dev/null 2>&1 &
+PID=`ps -eaf | grep $APP_FILE_NAME | grep -v grep | awk '{print $2}'`
+if [[ "" != "$PID" ]]; then
+  echo "killing $PID"
+  kill -9 $PID
+fi
+nohup java -server -Dspring.profiles.active=dev -jar $APP_FILE_NAME.jar > /dev/null 2>&1 &
