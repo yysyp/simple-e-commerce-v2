5dc3fb76a3f88215a0c3517cebf8842134ecb5c2
yysyp <yysyp@foxmail.com>
2023-10-19 12:24:44 +0800

    add script and test cache

---
+++ deployment-scripts/0env-set.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+set -o nounset
+set -o errexit
+#sed -i 's/\r$//' *.sh
+
+Project_ID=xxx
+Zone=xxx
+VM_NAME=my-vm-001
+SA_ACCOUNT=auto_deploy@xxx.com
+DEPLOY_FOLDER=deploy_folder
+LOGINUSER=ubuntu
+USERNAME=nexus1
+PASSWD=pass1
+
+echo "Env set. VM_NAME=$VM_NAME"
--- src/main/java/ps/demo/WebServerApplication.java
+++ src/main/java/ps/demo/WebServerApplication.java
@@ -23,6 +23,7 @@ public class WebServerApplication implements ApplicationRunner {
         log.info("--->>maxMemory={}m, totalMemory={}m, usedMemory={}m",
                 maxMemory / 1024 / 1024, totalMemory / 1024 / 1024,
                 (maxMemory - totalMemory) / 1024 / 1024);
+        log.info("System.getenv() = {}", System.getenv());
         SpringApplication.run(WebServerApplication.class, args);
     }
 
