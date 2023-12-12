d4a0820fe60ec1ee9031e6bc71bdc6beaae2d1ae
yysyp <yysyp@foxmail.com>
2023-12-12 22:31:05 +0800

    fix path

--- recopy-simple-e-commerce-v2-to-doc3.bat
+++ recopy-simple-e-commerce-v2-to-doc3.bat
@@ -1,5 +1,5 @@
-set "doc3-simple-e-commerce-v2=..\..\doc3\3-learn\33-coding\337-mytool-demo-snippet-code\simple-e-commerce-v2"
+set "simple-e-commerce-v2=..\doc3\3-learn\33-coding\337-mytool-demo-snippet-code\simple-e-commerce-v2"
 cd ..
-rd /S /Q %doc3-simple-e-commerce-v2%\
-robocopy doc3-simple-e-commerce-v2 %doc3-simple-e-commerce-v2%\ /E /MIR /Z /XD "not-copy" "upload-folder" "log" ".git" ".gitattributes" ".mvn"
-echo 'Copy current doc3-simple-e-commerce-v2 to doc3\3-learn\33-coding\337-mytool-demo-snippet-code\simple-e-commerce-v2 done!'
+rd /S /Q %simple-e-commerce-v2%\
+robocopy simple-e-commerce-v2 %simple-e-commerce-v2%\ /E /MIR /Z /XD "not-copy" "upload-folder" "log" ".git" ".gitattributes" ".mvn"
+echo 'Copy current simple-e-commerce-v2 to doc3\3-learn\33-coding\337-mytool-demo-snippet-code\simple-e-commerce-v2 done!'
