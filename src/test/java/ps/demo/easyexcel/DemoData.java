14993963a7cad7912fd8fe6a73db1ce167e4fb0b
yysyp <yysyp@foxmail.com>
2024-01-22 12:57:48 +0800

    add excel parse demo

--- src/test/java/ps/demo/easyexcel/DemoData.java
+++ src/test/java/ps/demo/easyexcel/DemoData.java
@@ -1,14 +1,22 @@
 package ps.demo.easyexcel;
 
+import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
 @Data
 public class DemoData {
 
-    @ExcelProperty(value = "StudentNo",index = 0)
+    @ExcelIgnore
+    private Integer id;
+
+    @ExcelProperty(value = "StudentNo", index = 0)
     private Integer sno;
 
     @ExcelProperty(value = "StudentName",index = 1)
     private String sname;
+
+    @ExcelProperty(value = "StudentAge",index = 2)
+    private Integer age;
+
 }
--- src/test/java/ps/demo/easyexcel/Main.java
+++ src/test/java/ps/demo/easyexcel/Main.java
@@ -3,39 +3,77 @@ package ps.demo.easyexcel;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.read.listener.ReadListener;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomUtils;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        String filename = "C:\\Users\\Dell\\2023-12-12_214012-myexcel.xlsx";
+        String filename = "C:\\Users\\Dell\\myexcel002.xlsx";
 
-        EasyExcel.write(filename, DemoData.class).sheet("Data list").doWrite(Main.getData());
+//        EasyExcel.write(filename, DemoData.class).sheet("Data list").needHead(true).doWrite(Main.getData());
+//
+//        List<DemoData> list = new ArrayList<>();
+//        EasyExcel.read(filename, DemoData.class, new ReadListener<DemoData>() {
+//            @Override
+//            public void invoke(DemoData o, AnalysisContext analysisContext) {
+//                list.add(o);
+//            }
+//
+//            @Override
+//            public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+//                //done!
+//            }
+//        }).sheet().headRowNumber(1).doRead();
+//
+//        System.out.println("-->(1)Read excel result=" + list);
 
-        List<DemoData> list = new ArrayList<>();
-        EasyExcel.read(filename, DemoData.class, new ReadListener<DemoData>() {
+        List<LinkedHashMap<String, Object>> dataList = new ArrayList<>();
+        EasyExcel.read(filename, new AnalysisEventListener<Map<String, Object>>() {
+            private Map<Integer, String> headMap;
+
             @Override
-            public void invoke(DemoData o, AnalysisContext analysisContext) {
-                list.add(o);
+            public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
+                this.headMap = headMap;
+            }
+
+            @Override
+            public void invoke(Map<String, Object> rowData, AnalysisContext analysisContext) {
+                LinkedHashMap<String, Object> dataMap = new LinkedHashMap<>();
+                for (int i = 0, n = rowData.size(); i < n; i++) {
+                    String key = headMap.get(i).trim();
+                    Object value = rowData.get(i);
+                    int keyPrefix = 0;
+                    String key1 = key;
+                    while (dataMap.containsKey(key1)) {
+                        keyPrefix++;
+                        key1 = key+keyPrefix;
+                    }
+                    dataMap.put(key1, value);
+                }
+                dataList.add(dataMap);
             }
 
             @Override
             public void doAfterAllAnalysed(AnalysisContext analysisContext) {
-                //done!
-            }
-        }).sheet().doRead();
 
-        System.out.println("-->Read excel result=" + list);
-        FileUtils.delete(new File(filename));
+            }
+        }).sheet().headRowNumber(1).doRead();
+
+        System.out.println("------------------------------------");
+        System.out.println("-->(2)Read excel dataList.0=" + dataList.get(0));
+        System.out.println("-->(2)Read excel dataList.size=" + dataList.size());
+        System.out.println("-->(2)Read excel dataList=" + dataList);
+        //FileUtils.delete(new File(filename));
     }
 
     public static List<DemoData> getData() {
@@ -44,6 +82,7 @@ public class Main {
             DemoData demoData = new DemoData();
             demoData.setSno(i);
             demoData.setSname("Test" + i);
+            demoData.setAge(RandomUtils.nextInt(10, 90));
             list.add(demoData);
         }
         return list;
